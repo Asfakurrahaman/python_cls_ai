906f44a427dd43d18292a02476691661fccc0e02
asfakur-rahaman <asfakurrahaman24@gmail.com>
2024-10-29 09:33:42 +0600

    commit_m

--- type_inheritance.py
+++ type_inheritance.py
@@ -2,17 +2,19 @@ class A:
     def display1(self):
         print("A")
 
-class B(A):
+class B:
     def display2(self):
         print("B")
 
 
-class C(B):
+class C:
     def display3(self):
         print("C")
-        pass
 
-ob1 = C()
+class D(A,B,C):
+    pass
+
+ob1 = D()
 ob1.display3()
 ob1.display2()
 ob1.display1()
